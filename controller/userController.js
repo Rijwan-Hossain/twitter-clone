4dfe4d528864d3031a070c7c6b2f0c19dbb168f3
Rijwan <rijyan.cse.152@gmail.com>
2019-10-27 12:09:44 +0600

    Login route successful

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,6 @@
 const User = require('../model/User') 
 const bcrypt = require('bcryptjs') 
+const jwt = require('jsonwebtoken')
 
 const registration = (req, res) => {
     // get data 
@@ -63,7 +64,74 @@ const registration = (req, res) => {
 } 
 
 
+const login = (req, res) => {
+    // get data 
+    // validate email 
+    // check email available or not 
+    // password match 
+    // create & return token 
+
+    let { email, password } = req.body; 
+
+    // validate email 
+    let i = 0, validate = false; 
+    for(i = 0; i < email.length; i++) { 
+        if(email[i] === '@') validate = true
+    }  
+    if(!validate) { 
+        return res.status(400).json({ 
+            status: 'Fail',
+            message: 'Enter a valid email'
+        }) 
+    } 
+
+    // check email available or not 
+    User.findOne({email}) 
+        .then(user => { 
+            if(!user) {
+                return res.json({
+                    status: 'Fail', 
+                    message: 'Invalid Email' 
+                }) 
+            } 
+
+            // Password match 
+            bcrypt.compare(password, user.password, (err, result) => {
+                if(err) {
+                    return res.json({ 
+                        error: 'Server Error' 
+                    }) 
+                } 
+
+                if(!result) {
+                    return res.json({ 
+                        status: 'Fail',
+                        message: 'Wrong Password' 
+                    }) 
+                } 
+
+                const payload = {
+                    id: user._id, 
+                    email: user.email 
+                } 
+
+                const token = jwt.sign(payload, 'SECRET', {expiresIn: '6h'}) 
+
+                return res.json({ 
+                    status: 'Success', 
+                    token: `Bearer ${token}`
+                }) 
+            })
+        }) 
+        .catch(err => { 
+            return res.json({ 
+                error: 'Server Error' 
+            }) 
+        }) 
+}
+
 module.exports = { 
-    registration 
+    registration, 
+    login
 } 
 
--- route/userRoute.js
+++ route/userRoute.js
@@ -1,9 +1,11 @@
 const express = require('express') 
 const route = express.Router() 
 const {
-    registration
+    registration, 
+    login
 } = require('../controller/userController')
 
 route.post('/registration', registration) 
+route.post('/login', login) 
 
 module.exports = route; 
